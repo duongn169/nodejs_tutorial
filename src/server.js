b7fc8d8f5dc0f337282303fbd9a173f24aa7ef17
DuongN <roxduong169@gmail.com>
2023-05-21 17:20:23 +0700

    video8 routing and MVC

--- src/server.js
+++ src/server.js
@@ -1,20 +1,19 @@
 
 import express from 'express';
-
+import initWebRoute from './route/web'
 import configViewEngine from './configs/viewEngine'
 require('dotenv').config();
 
 const app = express()
 const port =  process.env.PORT || 8080;
 
+// set up view engine
 configViewEngine(app);
 
-app.get('/', (req, res) =>{
-  res.render('index.ejs')
-})
-app.get('/about', (req, res) => {
-    res.send(`I'm Hoang Duong`)
-  })
+// init web route
+initWebRoute(app);
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
