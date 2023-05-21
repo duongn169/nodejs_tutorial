f14cad9f982bd92794a49ec83fe60bbee3c7f6cd
DuongN <roxduong169@gmail.com>
2023-05-21 11:53:37 +0700

    video5

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello world! </h3>');
-    res.write('<h2>Hoang Duong </h2>');
-    res.end();
+app.get('/', (req, res) => {
+  res.send('Hello World! vs Hoang Duong')
 })
-
-server.listen(3000, 'localhost', () => {
-    console.log('Node.JS server is running on port: 3000');
+app.get('/about', (req, res) => {
+    res.send(`I'm Hoang Duong`)
+  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
