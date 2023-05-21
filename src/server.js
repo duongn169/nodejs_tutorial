4e89a4a86b929e8e8409b7be6b4de685b292f05a
DuongN <roxduong169@gmail.com>
2023-05-21 13:20:09 +0700

    video6

---
+++ src/server.js
@@ -0,0 +1,20 @@
+
+import express from 'express';
+
+import configViewEngine from './configs/viewEngine'
+
+
+const app = express()
+const port = 8080
+
+configViewEngine(app);
+
+app.get('/', (req, res) =>{
+  res.render('index.ejs')
+})
+app.get('/about', (req, res) => {
+    res.send(`I'm Hoang Duong`)
+  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
